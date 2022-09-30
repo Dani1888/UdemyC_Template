325b9030be2b9746dccb977c17acbe469ecd4a8c
Dani1888 <93440506+Dani1888@users.noreply.github.com>
2022-09-30 19:58:18 +0200

    Variabeln anpassung

--- Chapter01_Introduction/main.c
+++ Chapter01_Introduction/main.c
@@ -4,6 +4,31 @@ int main()
 {
     printf("Hello World!\n");
 
+    float my_number_A = 15;
+    float my_nummer_B = 3;
+    float my_result;
+
+    my_result = my_number_A + my_nummer_B;
+    printf("float %f\n", my_result);
+
+    int A = 15;
+    int B = 3;
+    int R;
+    R = A--;
+
+
+    printf("int %d\n", R);
+
+    printf("int %d\n", R);
+
+    unsigned int A_unsignet_int = 15u;
+    unsigned int B_unsignet_int = 15u;
+    unsigned int R_unsignet_int;
+    R_unsignet_int = A_unsignet_int - 1u;
+
+
+    printf("int %u\n", R_unsignet_int);
+
+    printf("int %d\n", R_unsignet_int);
     return 0;
 }
-
